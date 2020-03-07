ea28a2fb726235965f9d2157032ae495d6dd1e9f
Adewale Orotayo <didymus7007@gmail.com>
2020-03-07 18:21:38 +0100

    link added but failing

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,30 @@
-require 'nokogiri'
+require 'httparty'
 require 'open-uri'
+require 'nokogiri'
+require 'pry'
 
-class Scraper
-  attr_accessor :link
+# page = HTTParty.get('https://newyork.craigslist.org/search/pet')
+# parse_page = Nokogiri::HTML(page)
 
-  def initialize(link)
-    @link = link
-    @page = open(@link)
-    @parse_page = Nokogiri::HTML(@page)
-  end
-end
+# doc = Nokogiri::HTML.parse(open("https://newyork.craigslist.org/search/pet/"))
+
+# link = 'https://www.aliexpress.com/category/200000668/shirts.html?spm=a2g0o.tm66085.102.6.47fd5f5bIPRwjf'
+
+link = 'https://www.aliexpress.com/'
+
+# link = 'https://newyork.craigslist.org/search/pet'
+page = Nokogiri::HTML.parse(open(link))
+
+
+Pry.start(binding)
+
+# class Scraper
+#   attr_accessor :link
+
+#   def initialize(link)
+#     @link = link
+#     @doc = Nokogiri::HTML(open(@link))
+#   end
+#   @doc.css()
+
+# end
