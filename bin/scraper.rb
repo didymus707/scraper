bd08acae5097eabde4fbdd3c6f90e4c95ee926b8
Adewale Orotayo <didymus7007@gmail.com>
2020-03-16 17:07:44 +0100

    Switched to string interpolation in line 17 of bin/scraper

--- bin/scraper.rb
+++ bin/scraper.rb
@@ -1,3 +1,4 @@
+#!/usr/bin/env ruby
 require './lib/scraper_logic'
 
 scraper = Scraper.new
@@ -8,12 +9,11 @@ program.each_with_index do |(course, _), index|
   puts "#{index.next}. #{course}"
 end
 
-
 puts '+-' * 15
 print "\nSelect the number of the course you want to open up: > "
 answer = gets.to_i
 puts '+-' * 15
 puts
-puts 'You chose %i: %s' % [answer, program.keys[answer - 1]]
+puts "You chose #{answer}: #{program.keys[answer - 1]}"
 puts 'Content covered in course:'
 puts "\n #{program[keys[answer - 1]].join(',')}"
